19b171dd283265981469452fe0d96f0efe92f4e2
brunoap62 <brunoap62@gmail.com>
2026-09-05 20:12:39 -0300

    Add CDN configuration and tags for CloudFront and S3 modules

--- terraform/modules/cloudfront/variables.tf
+++ terraform/modules/cloudfront/variables.tf
@@ -7,3 +7,15 @@ variable bucket_domain_name {
   type        = string
   description = "The domain name of the S3 bucket"
 }
+
+variable "cdn_price_class" {
+  type        = string
+  default     = "PriceClass_200"
+  description = "Classe de preço do CDN"
+}
+
+variable "cdn_tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags de criação"
+}
