5d3fd9aca13682fe9f2c410996a8f34d1c85292c
brunoap62 <brunoap62@gmail.com>
2026-09-05 19:57:01 -0300

    Add CloudFront module and outputs for S3 bucket integration

---
+++ terraform/modules/cloudfront/variables.tf
@@ -0,0 +1,9 @@
+variable origin_id {
+  type        = string
+  description = "The ID of the S3 bucket"
+}
+
+variable bucket_domain_name {
+  type        = string
+  description = "The domain name of the S3 bucket"
+}
