6f00bff3f83078dcbb3e4ed7650e2d7cd518a603
brunoap62 <brunoap62@gmail.com>
2026-09-06 17:24:18 -0300

    Add variables for project name, cluster name, and AWS region

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+  description = "Nome do projeto"
+  type        = string
+  default     = "jojo-adventure"
+}
+
+variable "cluster_name" {
+  description = "Nome do cluster EKS"
+  type        = string
+  default     = "jojo-eks-cluster"
+}   
+
+  variable "aws_region" {                                                                                                                                  
+      description = "Região AWS padrão"                                                                                                                      
+      type        = string                                                                                                                                   
+      default     = "us-east-1"                                                                                                                              
+    }   
