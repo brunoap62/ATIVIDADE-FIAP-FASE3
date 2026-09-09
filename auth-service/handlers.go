aae81bc4ea9ce85d43fba9a6c4bd8cdc86f570ca
brunoap62 <brunoap62@gmail.com>
2026-09-08 22:10:55 -0300

    fix: update health check response message to include 'argo Cd'

--- auth-service/handlers.go
+++ auth-service/handlers.go
@@ -25,7 +25,7 @@ type CreateKeyResponse struct {
 func (a *App) healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	_ = json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	_ = json.NewEncoder(w).Encode(map[string]string{"status": "ok argo Cd"})
 }
 
 // validateKeyHandler verifica se uma chave de API (enviada via Header) é válida
