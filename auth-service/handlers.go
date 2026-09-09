c1d4803c492e4b0ce6ee6ad2413c1cdf195404dc
brunoap62 <brunoap62@gmail.com>
2026-09-09 09:24:10 -0300

    fix: update health check response message to include 'ok Teste 3m'

--- auth-service/handlers.go
+++ auth-service/handlers.go
@@ -25,7 +25,7 @@ type CreateKeyResponse struct {
 func (a *App) healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	_ = json.NewEncoder(w).Encode(map[string]string{"status": "ok ssm"})
+	_ = json.NewEncoder(w).Encode(map[string]string{"status": "ok Teste 3m"})
 }
 
 // validateKeyHandler verifica se uma chave de API (enviada via Header) é válida
