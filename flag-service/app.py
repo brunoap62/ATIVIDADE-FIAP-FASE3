b3a840b15dbda7ee03417f95aee880bb76c03516
brunoap62 <brunoap62@gmail.com>
2026-09-10 16:53:23 -0300

    feat(health-check): update health endpoint response message

--- flag-service/app.py
+++ flag-service/app.py
@@ -112,7 +112,7 @@ def require_auth(f):
 
 @app.route('/health')
 def health():
-    return jsonify({"status": "ok"})
+    return jsonify({"status": "ok flag teste"})
 
 @app.route('/flags', methods=['POST'])
 @app.route('/', methods=['POST'])
